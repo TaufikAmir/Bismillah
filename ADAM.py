ccdc0b37fe5932d15e949e13b291e82a85c97eb2
TaufikAmir <taufikamir00@gmail.com>
2025-05-09 11:13:39 +0800

    Update ADAM.py

--- ADAM.py
+++ ADAM.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 
 
 
-st.header("Advanced corrodeD pipe structurAl integrity systeM (ADAM)")
+st.header("Advanced corrodeD pipe structurAl integrity systeM Fatigue AssessmenT Instrumentation for corroded pipe Hydrostatic burst pressure (ADAM FATIH)")
 
 st.subheader('Dimensional Parameters')
 htp="https://www.researchgate.net/profile/Changqing-Gong/publication/313456917/figure/fig1/AS:573308992266241@1513698923813/Schematic-illustration-of-the-geometry-of-a-typical-corrosion-defect.png"
